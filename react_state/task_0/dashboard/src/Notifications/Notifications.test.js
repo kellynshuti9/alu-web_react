dbe1fb772169d00fe753ab428ed765ff4d04b291
kellynshuti9 <k.dushimima@alustudent.com>
2026-09-24 23:51:45 +0200

    intrane project

--- react_state/task_0/dashboard/src/Notifications/Notifications.test.js
+++ react_state/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -78,4 +78,33 @@ describe('<Notifications />', () => {
         instance.markAsRead(1);
         expect(spy).toHaveBeenCalledWith('Notification 1 has been marked as read');
     })
+
+    it('verifies that clicking on the menu item calls handleDisplayDrawer', () => {
+        const handleDisplayDrawer = jest.fn();
+        const handleHideDrawer = jest.fn();
+        const wrapper = shallow(
+            <Notifications
+                displayDrawer={ false }
+                handleDisplayDrawer={ handleDisplayDrawer }
+                handleHideDrawer={ handleHideDrawer }
+            />
+        );
+        wrapper.find('.menuItem').simulate('click');
+        expect(handleDisplayDrawer).toHaveBeenCalled();
+    });
+
+    it('verifies that clicking on the close button calls handleHideDrawer', () => {
+        const handleDisplayDrawer = jest.fn();
+        const handleHideDrawer = jest.fn();
+        const wrapper = shallow(
+            <Notifications
+                displayDrawer={ true }
+                handleDisplayDrawer={ handleDisplayDrawer }
+                handleHideDrawer={ handleHideDrawer }
+                listNotifications={ listNotifications }
+            />
+        );
+        wrapper.find('button').simulate('click');
+        expect(handleHideDrawer).toHaveBeenCalled();
+    });
 });
